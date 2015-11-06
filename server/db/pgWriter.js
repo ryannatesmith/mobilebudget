7ee2f1e9839f71e1787359a09bbc5f2af65ed06b
Ryan Nate Smith <natesmith@yandex.ru>
2015-11-05 18:54:39 -0800

    Created a file to start the server, and began working on files to connect to the database and query it

---
+++ server/db/pgWriter.js
@@ -0,0 +1,20 @@
+var pg = require('pg');
+var config = require('config.js');
+
+var PGSqlError = function(message) {
+	this.name 		= "PGSqlError";
+	this.message 	= message;
+}
+
+//set up the connection string
+var conString = "postgres://" + config.user + ":" + config.password + "@" + config.host + "/" + config.database;
+
+var client = new pg.Client(constring);
+
+return client;
+
+//client.connect(function(err) {
+//	if(err) {
+//		throw new PGSqlError("Unable to connect to the database. \n" + err);
+//	}
+//}
